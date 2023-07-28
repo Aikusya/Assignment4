d7b125673041406a26fe5a1dff37ee391d326615
Aikusya <butterflyy1211@gmail.com>
2023-07-28 13:39:53 +0600

    search

---
+++ src/Search.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Search<V> {
+    protected MyGraph<V> graph;
+
+    public Search(MyGraph<V> graph) {
+        this.graph = graph;
+    }
+
+    // No additional methods to complete for the Search class in this context
+}
