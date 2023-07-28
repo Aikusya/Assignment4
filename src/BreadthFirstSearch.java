b4ef467974e4b709b1658f3f50b6b57f2b0d61e6
Aikusya <butterflyy1211@gmail.com>
2023-07-28 13:41:00 +0600

    BreadthFirstSearch

---
+++ src/BreadthFirstSearch.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class BreadthFirstSearch<V> extends Search<V> {
+
+    public BreadthFirstSearch(MyGraph<V> graph) {
+        super(graph);
+    }
+
+    public List<Vertex<V>> bfsTraversal(Vertex<V> startVertex) {
+        List<Vertex<V>> result = new ArrayList<>();
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        Set<Vertex<V>> visited = new HashSet<>();
+
+        queue.offer(startVertex);
+        visited.add(startVertex);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> currentVertex = queue.poll();
+            result.add(currentVertex);
+
+            for (Vertex<V> neighbor : graph.getAdjacentVertices(currentVertex)) {
+                if (!visited.contains(neighbor)) {
+                    queue.offer(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
